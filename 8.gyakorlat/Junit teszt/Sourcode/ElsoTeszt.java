8976b851a47868d9336824ef87ce155fee7fda13
megvagyhadnagy01 <38715272+megvagyhadnagy01@users.noreply.github.com>
2018-11-14 10:12:04 +0100

    br

---
+++ 8.gyakorlat/Junit teszt/Sourcode/ElsoTeszt.java
@@ -0,0 +1,22 @@
+package gkiss01.binfa;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ElsoTeszt {
+
+	LZWBinfa binFa = new LZWBinfa();
+
+	@Test
+	void test() {
+		
+		for(char c : "01111001001001000111".toCharArray()) {
+            binFa.belerak(c);
+        }
+
+        assertEquals(4, binFa.getMelyseg(), 0.000001);
+        assertEquals(2.75, binFa.getAtlag(), 0.001);
+        assertEquals(0.957427, binFa.getSzoras(), 0.0001);
+	}
+}
